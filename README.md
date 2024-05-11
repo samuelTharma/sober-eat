128c940cf188816feb3cf7b7b87a845d38306db8
unknown <satheeskumar@gmail.com>
2024-05-11 15:12:28 +0100

    add order-service

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sober-eat
+# sober-ear 
+build with java 17
+
+# sober-eat/ order-service
+
+to get the mock order joson
+http://localhost:8080/sober-eat/order-service/get
+
+to create order
+http://localhost:8080/sober-eat/order-service/create
---
+++ order-service/src/main/java/com/sobereat/orderservice/impl/OrderServiceApiDelegateImpl.java
@@ -0,0 +1,46 @@
+package com.sobereat.orderservice.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sobereat.orderservice.rest.api.OrderServiceApiDelegate;
+import com.sobereat.orderservice.rest.model.Customer;
+import com.sobereat.orderservice.rest.model.Menu;
+import com.sobereat.orderservice.rest.model.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderServiceApiDelegateImpl implements OrderServiceApiDelegate
+{
+  @Override
+  public ResponseEntity<Order> create(Order order)
+  {
+    System.out.println("order " + order);
+    return ResponseEntity.ok(order);
+  }
+
+  @Override
+  public ResponseEntity<Order> find()
+  {
+    System.out.println(" getting order ..... ");
+    Order order1 = new Order();
+    List<Object> menuList = new ArrayList<>();
+    Menu menu1 = new Menu();
+    menu1.setId("1");
+    menu1.setName("pizza");
+    Menu menu2 = new Menu();
+    menu2.setId("2");
+    menu2.setName("bread");
+    order1.setId("11");
+    menuList.add(menu1);
+    menuList.add(menu2);
+    order1.setMenu(menuList);
+    Customer customer = new Customer();
+    customer.setName("John");
+    customer.setAddress("22, London Street, L29 7TU");
+    customer.setPhoneNumber("676676722");
+    order1.setCustomer(customer);
+    return ResponseEntity.ok(order1);
+  }
+}
